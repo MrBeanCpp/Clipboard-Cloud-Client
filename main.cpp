5ad0305b70efcb74147577c57919f52aea8cf8cf
mrbeanc <mrbeanc@qq.com>
2024-03-20 11:41:38 +0800

    禁用全局代理 NoProxy
    为什么Qt默认走代理啊喂！

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "widget.h"
-
 #include <QApplication>
+#include <QNetworkProxy>
 
 int main(int argc, char *argv[])
 {
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     QApplication::setQuitOnLastWindowClosed(false); //否则，在窗口没有show的时候，弹窗，并关闭弹窗，程序居然会退出
+    QNetworkProxy::setApplicationProxy(QNetworkProxy::NoProxy); //禁用全局代理，为什么Qt默认走代理啊喂！
     Widget w;
     // w.show();
     return a.exec();
