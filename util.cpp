60f710f2e7f915a2c20f81e7b359c2ea65c47371
mrbeanc <mrbeanc@qq.com>
2024-06-12 15:59:31 +0800

    support QQ Gif
    read from file.
    how to read full .gif from clipboard ?

--- util.cpp
+++ util.cpp
@@ -9,6 +9,7 @@
 #include <QBuffer>
 #include <QClipboard>
 #include <QUuid>
+#include <QRegularExpression>
 
 const QString Util::REG_AUTORUN = "HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"; //HKEY_CURRENT_USER仅仅对当前用户有效，但不需要管理员权限
 
@@ -63,6 +64,34 @@ QByteArray Util::clipboardData(bool* isText)
         *isText = !clipData->hasImage(); //有可能同时hasText，所以以image为准
     }
     if (clipData->hasImage()) {
+        static auto extractFilePath = [](const QString& str) {
+            static QRegularExpression re("filepath=\"([^\"]*)\"");
+            QRegularExpressionMatch match = re.match(str);
+            if (match.hasMatch()) {
+                return match.captured(1);
+            } else {
+                return QString();
+            }
+        };
+
+        // 貌似不同软件的mime格式不一样，这里只处理QQ
+        // "<QQRichEditFormat><Info version=\"1001\"></Info><EditElement type=\"1\" imagebiztype=\"7\" textsummary=\"[崇拜]\"
+        // filepath=\"E:\\xxx\\Tencent Files\\xxx\\Image\\C2C\\Image8\\a611951b0d...681d24c21a94c.gif\" shortcut=\"\">
+        // </EditElement></QQRichEditFormat>"
+        auto qqRichData = QString::fromUtf8(clipData->data("application/x-qt-windows-mime;value=\"QQ_Unicode_RichEdit_Format\""));
+        auto imagePath = extractFilePath(qqRichData); // 正则提取路径
+        // .gif 特殊处理，因为QImage不支持gif的Write，如果直接转换为jpg只会保留一帧
+        if (imagePath.endsWith(".gif")) {
+            QFile file(imagePath);
+            if (file.open(QIODevice::ReadOnly)) {
+                data = file.readAll();
+                file.close();
+                return data;
+            } else {
+                qCritical() << "Unable to open the file:" << imagePath;
+            }
+        }
+
         //format: application/x-qt-image
         QImage image = qvariant_cast<QImage>(clipData->imageData()); //不能直接toByteArray，需要先转换为QImage，否则为空
         if (!image.isNull()) {
@@ -71,7 +100,8 @@ QByteArray Util::clipboardData(bool* isText)
             image.save(&buffer, "jpg"); // 将 QImage 保存为 jpg 格式
             buffer.close();
             // data = data.toBase64(); // 转换为 Base64 编码，防止老式设备进行隐式编解码导致信息丢失
-        }
+        } else
+            qWarning() << "WARN: QImage is null";
     } else if (clipData->hasText()) {
         data = clipData->text().toUtf8();
     } else {
